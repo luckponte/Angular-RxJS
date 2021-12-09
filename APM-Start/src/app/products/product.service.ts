2ad8dc598bbc72f47b30625b4f45709f9ceca0ef
luckponte <luckpontecorreia@gmail.com>
2021-12-09 10:26:18 -0300

    Increase prices like the greedy bastards we are

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { EMPTY, Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -15,12 +15,17 @@ export class ProductService {
   private productsUrl = 'api/products';
   private suppliersUrl = this.supplierService.suppliersUrl;
 
-  products$ = this.http
-    .get<Product[]>(this.productsUrl)
-    .pipe(
-      tap((data) => console.log('Products: ', JSON.stringify(data))),
-      catchError(this.handleError)
-    );
+  products$ = this.http.get<Product[]>(this.productsUrl).pipe(
+    map((data) =>
+      data.map((product) => ({
+        ...product,
+        price: product.price * 1.5,
+        searchKey: [product.productName],
+      }) as Product)
+    ),
+    tap((data) => console.log('Products: ', JSON.stringify(data))),
+    catchError(this.handleError)
+  );
 
   constructor(
     private http: HttpClient,
